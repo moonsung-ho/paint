59616e8096dfb810bad6341fc567509cac99fff0
sungho Moon <moon@7959.ml>
2020-10-01 18:28:25 +0900

    gk

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-ctx.fillStyle = "white";
 ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = "INITIAL_COLOR";
 ctx.fillStyle = "INITIAL_COLOR";
